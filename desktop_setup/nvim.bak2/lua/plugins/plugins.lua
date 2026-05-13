5eb95db711fba0a00a61455dc5111c0a9c76fc9c
ICE-BREAKER2353 <said2353@hotmail.com>
2026-05-13 17:55:13 -0400

    added potential future new nvim config

---
+++ desktop_setup/nvim.bak2/lua/plugins/plugins.lua
@@ -0,0 +1,154 @@
+-- return {
+--   {
+--     -- LSP Configuration & Plugins
+--     'neovim/nvim-lspconfig',
+--     dependencies = {
+--       -- Automatically install LSPs to stdpath for neovim
+--       'williamboman/mason.nvim',
+--       'williamboman/mason-lspconfig.nvim',
+--
+--       -- Useful status updates for LSP
+--       'j-hui/fidget.nvim',
+--
+--       -- Additional lua configuration, makes nvim stuff amazing
+--       'folke/neodev.nvim',
+--     },
+--   },
+--   {
+--     "folke/zen-mode.nvim",
+--     config = function()
+--       require("zen-mode").setup {
+--         -- your configuration comes here
+--         -- or leave it empty to use the default settings
+--         -- refer to the configuration section below
+--       }
+--     end
+--   },
+--   {
+--     "folke/noice.nvim",
+--     config = function()
+--       require("noice").setup({
+--         lsp = {
+--           -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+--           override = {
+--             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--             ["vim.lsp.util.stylize_markdown"] = true,
+--             ["cmp.entry.get_documentation"] = true,
+--           },
+--         },
+--         -- you can enable a preset for easier configuration
+--         presets = {
+--           bottom_search = true,         -- use a classic bottom cmdline for search
+--           command_palette = true,       -- position the cmdline and popupmenu together
+--           long_message_to_split = true, -- long messages will be sent to a split
+--           inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+--           lsp_doc_border = false,       -- add a border to hover docs and signature help
+--         },
+--       })
+--     end,
+--     dependencies = {
+--       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+--       "MunifTanjim/nui.nvim",
+--       -- OPTIONAL:
+--       --   `nvim-notify` is only needed, if you want to use the notification view.
+--       --   If not available, we use `mini` as the fallback
+--       "rcarriga/nvim-notify",
+--     }
+--   },
+--   {
+--     'akinsho/toggleterm.nvim', version = "*", config = true
+--   },
+--   {
+--     -- Highlight, edit, and navigate code
+--     'nvim-treesitter/nvim-treesitter',
+--     build = function()
+--       pcall(require('nvim-treesitter.install').update { with_sync = true })
+--     end,
+--       dependencies = {
+--         'nvim-treesitter/nvim-treesitter-textobjects',
+--       },
+--     config = function ()
+--       require('user.treesitter')
+--     end,
+--   },
+--   { -- Additional text objects via treesitter
+--     'nvim-treesitter',
+--   },
+--   -- Git related plugins
+--   'tpope/vim-fugitive',
+--   'tpope/vim-rhubarb',
+--   'mfussenegger/nvim-jdtls',
+--   'mfussenegger/nvim-dap',
+--   {
+--     'mfussenegger/nvim-dap-python',
+--     ft = "python",
+--
+--     dependencies = {
+--       "mfussenegger/nvim-dap",
+--     },
+--     -- config = function(_, opts)
+--     --   local path = "C:/Users/Amin Said/AppData/Local/nvim-data/mason/packages/debugpy/venv/Scripts"
+--     --   require("dap-python").setup(path)
+--     -- end,
+--   },
+--   'rcarriga/cmp-dap',
+--   "folke/neodev.nvim",
+--   'simrat39/rust-tools.nvim',
+--   'xiyaowong/transparent.nvim',
+--   {
+--     'stevearc/dressing.nvim',
+--     opts = {},
+--   },
+--   'jose-elias-alvarez/null-ls.nvim',
+--   'navarasu/onedark.nvim', -- Theme inspired by Atom
+--   'Mofiqul/dracula.nvim',
+--   'folke/lsp-colors.nvim',
+--   {
+--     "catppuccin/nvim", name = "catppuccin"
+--   },
+--   'water-sucks/darkrose.nvim',
+--   'rmehri01/onenord.nvim',
+--   'tpope/vim-sleuth',          -- Detect tabstop and shiftwidth automatically
+--   'mbbill/undotree',
+--   -- Fuzzy Finder (files, lsp, etc)
+--   {
+--     'nvim-telescope/telescope.nvim',
+--     branch = 'master',
+--     dependencies = {
+--       'nvim-lua/plenary.nvim'
+--     }
+--   },
+--   -- Fuzzy Finder Algorithm which dependencies local dependencies to be built. Only load if `make` is available
+--   {
+--     'nvim-telescope/telescope-fzf-native.nvim',
+--     build = 'make',
+--     cond = vim.fn.executable 'make' == 1
+--   },
+--   {
+--     'nvim-telescope/telescope-dap.nvim',
+--     dependencies = { { 'mfussenegger/nvim-dap' },
+--       { 'rcarriga/nvim-dap-ui' }, { 'Pocco81/DAPInstall.nvim', opt = true } },
+--     config = [[require('dap.config')]],
+--     ft = { 'java', 'python', 'rust', 'go' }
+--   },
+--   'leoluz/nvim-dap-go',
+--   {
+--     'nvim-orgmode/orgmode',
+--     config = function()
+--       require('orgmode').setup {}
+--     end
+--   },
+--   {
+--     "folke/which-key.nvim",
+--     config = function()
+--       vim.o.timeout = true
+--       vim.o.timeoutlen = 300
+--       require("which-key").setup {
+--         -- your configuration comes here
+--         -- or leave it empty to use the default settings
+--         -- refer to the configuration section below
+--       }
+--     end
+--   },
+--   'onsails/lspkind.nvim',
+-- }
